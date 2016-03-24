5d0d50ab45e5a8ecc3240b1fe59da4c6286001a5
Amit_Kaspi <akaspi@wix.com>
2016-03-24 12:42:42 +0200

    Initial Commit

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+'use strict';
+
+import React from 'react';
+
+module.exports = React.createClass({
+    render: () => {
+        return (
+            <div>THIS IS THE APP</div>
+        )
+    }
+});
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var webpack = require('webpack');
+
+module.exports = {
+    entry: [
+        'webpack-dev-server/client?http://localhost:8080',
+        'webpack/hot/only-dev-server',
+        './src/index.jsx'
+    ],
+    output: {
+        path: __dirname + '/dist',
+        publicPath: '/dist',
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            { test: /\.jsx?$/, exclude: /node_modules/, loader: 'react-hot!babel' },
+            { test: /\.scss$/, exclude: /node_modules/, loaders: ['style', 'css', 'resolve-url', 'sass'] }
+        ]
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
+    devServer: {
+        contentBase: './dist',
+        hot: true
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin()
+    ]
+};
