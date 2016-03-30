399f35c840529c8060a792a97816adf2e8ca3c3a
Amit_Kaspi <akaspi@wix.com>
2016-03-30 08:57:47 +0300

    Fix webpack-dev-server issue

--- webpack.config.js
+++ webpack.config.js
@@ -8,27 +8,25 @@ module.exports = {
         'webpack/hot/only-dev-server',
         './src/index.jsx'
     ],
-    output: {
-        path: __dirname + '/dist',
-        publicPath: '/dist',
-        filename: 'bundle.js'
-    },
     module: {
         loaders: [
             { test: /\.jsx?$/, exclude: /node_modules/, loader: 'react-hot!babel' },
-            { test: /\.scss$/, exclude: /node_modules/, loaders: ['style', 'css', 'resolve-url', 'sass'] }
-        ]
+            { test: /\.scss$/, loaders: ['style', 'css', 'resolve-url', 'sass']
+        }]
     },
     resolve: {
         extensions: ['', '.js', '.jsx']
+    },
+    output: {
+        path: __dirname + '/dist',
+        publicPath: '/',
+        filename: 'bundle.js'
     },
     devServer: {
         contentBase: './dist',
         hot: true
     },
-    devtool: "#inline-source-map",
     plugins: [
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.NoErrorsPlugin()
+        new webpack.HotModuleReplacementPlugin()
     ]
 };
